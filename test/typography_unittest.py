96ef5f0f96d3474e560bd992285f1d8e831878ed
Norman Markgraf <nmarkgraf@hotmail.com>
2018-04-01 22:27:31 +0200

    Neue Release: 1.0.0 --- Komplett Umgeschrieben.

--- test/typography_unittest.py
+++ test/typography_unittest.py
@@ -1,2 +1,166 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+'''
+  Quick-Typographie-Filter unit test: typography_unittest.py
+
+  (C)opyleft in 2018 by Norman Markgraf (nmarkgraf@hotmail.com)
+
+  Release:
+  ========
+  0.1   - 01.04.2018 (nm) - Erste Version
+
+
+  WICHTIG:
+  ========
+    Benoetigt python3 !
+    -> https://www.howtogeek.com/197947/how-to-install-python-on-windows/
+    oder
+    -> https://www.youtube.com/watch?v=dX2-V2BocqQ
+
+  RUN THE TESTS:
+  ==============
+  Um die unit tests auszuführen kann man im Terminal den Befehl
+
+    > python3 -m unittest test/style_unittest.py
+
+  im Hauptverzeichnis des Projektes eingeben!
+
+  Lizenz:
+  =======
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+  "Errare (Errasse) humanum est, sed in errare (errore) perseverare diabolicum."
+
+    -- Cicero
+
+'''
+
+# import sys
+import unittest
+
+# sys.path.append('..')
+import panflute as pf  # panflute fuer den pandoc AST
+from typography import *
+
+
+class StyleTest(unittest.TestCase):
+
+    def test_makeInline_InstanceHTML(self):
+        input = ("xxx", "yyy")
+        result = pf.RawInline
+        self.assertIsInstance(makeInline(input, "html"), result)
+
+
+    def test_makeInline_InstanceLaTeX(self):
+        input = ("xxx", "yyy")
+        result = pf.RawInline
+        self.assertIsInstance(makeInline(input, "latex"), result)
+
+
+    def test_makeInline_InstanceBeamer(self):
+        input = ("xxx", "yyy")
+        result = pf.RawInline
+        self.assertIsInstance(makeInline(input, "beamer"), result)
+
+
+    def test_makeInline_FormatHTML(self):
+        input = ("xxx", "yyy")
+        result = "html"
+        self.assertEqual(makeInline(input, "html").format, result)
+
+
+    def test_makeInline_FormatLaTeX(self):
+        input = ("xxx", "yyy")
+        result = "latex"
+        self.assertEqual(makeInline(input, "latex").format, result)
+
+
+    def test_makeInline_FormatBeamer(self):
+        input = ("xxx", "yyy")
+        result = "latex"
+        self.assertEqual(makeInline(input, "beamer").format, result)
+
+
+    def test_makeInline_OutputHTML2(self):
+        input = ("xxx", "yyy")
+        result = "xxx&thinsp;yyy"
+        self.assertEqual(makeInline(input, "html").text, result)
+
+
+    def test_makeInline_OutputHTML3(self):
+        input = ("xxx", "yyy", "zzz")
+        result = "xxx&thinsp;yyy&thinsp;zzz"
+        self.assertEqual(makeInline(input, "html").text, result)
+
+
+    def test_makeInline_OutputLaTeX2(self):
+        input = ("xxx", "yyy")
+        result = "\\mbox{xxx\\thinspace{}yyy}\\xspace{}"
+        self.assertEqual(makeInline(input, "latex").text, result)
+
+
+    def test_makeInline_OutputLaTeX3(self):
+        input = ("xxx", "yyy", "zzz")
+        result = "\\mbox{xxx\\thinspace{}yyy\\thinspace{}zzz}\\xspace{}"
+        self.assertEqual(makeInline(input, "latex").text, result)
+
+    def test_makeInline_OutputBeamer2(self):
+        input = ("xxx", "yyy")
+        result = "\\mbox{xxx\\thinspace{}yyy}\\xspace{}"
+        self.assertEqual(makeInline(input, "beamer").text, result)
+
+
+    def test_makeInline_OutputBeamer3(self):
+        input = ("xxx", "yyy", "zzz")
+        result = "\\mbox{xxx\\thinspace{}yyy\\thinspace{}zzz}\\xspace{}"
+        self.assertEqual(makeInline(input, "beamer").text, result)
+
+
+    def test_makeLaTeXEscapes1(self):
+        str = "$"
+        result = "\\$"
+        self.assertEqual(makeLaTeXEscapes(str), result)
+
+
+    def test_makeLaTeXEscapes2(self):
+        str = "%"
+        result = "\\%"
+        self.assertEqual(makeLaTeXEscapes(str), result)
+
+
+    def test_makeLaTeXEscapes3(self):
+        str = "US-$"
+        result = "US-\\$"
+        self.assertEqual(makeLaTeXEscapes(str), result)
+
+
+    def test_isThisSpace1(self):
+        self.assertTrue(isThisSpace(pf.Space()))
+
+
+    def test_isThisSpace2(self):
+        self.assertFalse(isThisSpace(pf.Str("")))
+
+
+    def test_isThisString1(self):
+        self.assertTrue(isThisString(pf.Str("")))
+
+
+    def test_isThisString2(self):
+        self.assertFalse(isThisString(pf.Space()))
+
+
+if __name__ == "__main__":
+    unittest.main()
--- typography.py
+++ typography.py
@@ -8,28 +8,8 @@
 
   Release:
   ========
-  0.1   - 25.10.2017 (nm) - Erste Version
-  0.2   - 25.10.2017 (rm) - Verbesserte Version
-  0.3   - 10.11.2017 (nm) - Erweiterte Version
-  0.4   - 28.11.2017 (nm) - Erweiterte Version
-  0.5   - 08.12.2017 (nm) - Erste Versuche mit mbox und xspace
-  0.6   - 11.12.2017 (nm) - "thinspace " statt "\," in LaTeX
-  0.7   - 17.12.2017 (se) - Neue Abkürzungen eingeführt.
-  0.8   - 18.12.2017 (nm) - Teilweise Umstellen auf RegEx.
-  0.9   - 19.12.2017 (nm) - Noch weiter auch RegEx umgestellt.
-  0.9.1 - 18.01.2018 (nm) - Ausdrücke wie "(I/ II)" wurden nicht richtig erkannt.
-                            Ergebnis "I/ II)"! - gefixed!
-  0.9.2 - 08.02.2018 (nm) - Jetzt wird auf "z.~B." etc. erkannt und korrigiert.
-  0.9.3 - 09.02.2018 (nm) - HotBugFix-Release!
-  0.9.4 - 10.02.2018 (nm) - Zitationen wie "S. 211" oder "S. 211 f." und "S. 211 ff."
-                            werden nun durch halbe Leerzeichen getrennt.
-                            (Funktioniert _nicht_ im Literaturverzeichnissen die von LaTeX
-                             automatisch erzeugt werden!!!)
-  0.9.4 - 11.02.2018 (nm) - Dokumentation erweitert.
-  0.9.5 - 14.03.2018 (nm) - Datumsangaben werden nach Typographieregeln nicht mit einem
-                            halben Leerzeichen getrennt. Sondern ganz nicht! - Jetzt korrekt
-                            implementiert.
-  0.9.6 - 14.03.2018 (nm) - Temperatur-, cm-, m-, mm-, km-, ccm-, und Euro-Angaben korrigieren.
+  1.0.0 - 01.04.2018 (nm) - Neue Fassung aller Module
+
 
   WICHTIG:
   ========
@@ -79,11 +59,9 @@ import re as re  # re fuer die Regulaeren Ausdruecke
 import logging  # logging fuer die 'typography.log'-Datei
 
 
-'''
- Eine Log-Datei "typography.log" erzeugen um einfacher zu debuggen
-'''
-#logging.basicConfig(filename='typography.log', level=logging.ERROR)
-logging.basicConfig(filename='typography.log', level=logging.DEBUG)
+# Eine Log-Datei "typography.log" erzeugen um einfacher zu debuggen
+DEBUGLEVEL = logging.ERROR  # DEBUG  oder ERROR or INFO
+logging.basicConfig(filename='typography.log', level=DEBUGLEVEL)
 
 
 '''
@@ -101,66 +79,11 @@ xspace = "\\xspace{}"
 
 
 '''
-
+ LaTeXt-mbox Begin und Ende
 '''
 beginBox = "\\mbox{"
 endBox = "}"
 
-'''
- Dieses Pattern sollte
- x.y. / (x.y. / (x.y.: / (x.y.) / x.y.: ...
- für alle Buchstaben x und y abdecken.
- Wichtig ... \D, damit keine Datumsangaben in die Mangel genommen werden!
- So soll z.B. 15.9.  eben nicht in Muster fallen!
-'''
-pattern1 = "([\(,\[,<,\{]?\w\.)(?:[~|\xa0]?)(\D\.[\),\],>]?[:,\,,\.,\!,\?]?[\),\],\},>]?)"
-
-
-'''
- Dieses Pattern sollte alle / am Ende eines Strings finden.
-'''
-pattern2 = "([\w|\(|\[|\{]+)\/$"
-
-
-'''
- Diesen Pattern soll pruefen ob der vorherige und nachfolgende Text
- zu einem halben Leerzeichen fuehrt.
-'''
-pattern3a = "([\(,\[,<,\{]?\w\.)"
-pattern3b = "^(\w\.[\),\],>]?[:,\,,\.,\!,\?]?[\),\],\},>]?)$"
-
-
-'''
- Aufspueren von Seitenangaben: "211." und "211-212."
-'''
-pattern4 = "\d+[-\d+]?\.?"
-
-'''
- Aufspueren von Seitenforsetzungsmarkierungen: "211 f." und "211 ff."
-'''
-pattern5 = "^ff?\.?$"
-
-
-'''
- Aufspueren von Temperaturangaben mit °C oder °F in der Form 21°C korrigieren
-'''
-pattern6 = "([-|+]?\d+,?-{0,2})(K[.,;:]?|°F[.,;:]?|°C[.,;:]?|€[.,;:]?|T€[.,;:]?|EUR[.,;:]?|Euro[.,;:]?|kg[.,;:]?|g[.,;:]?|km[.,;:]?|m[.,;:]?|Meter[.,;:]?|cm[.,;:]?|mm[.,;:]?|ccm[.,;:]?|\$[.,;:]?|US\-\$[.,;:]?)$"
-
-'''
- Ab hier werden die Muster von oben voruebersetzt:
-'''
-
-recomp1 = re.compile(pattern1)
-
-recomp2 = re.compile(pattern2)
-
-recomp3a = re.compile(pattern3a)
-recomp3b = re.compile(pattern3b)
-
-recomp4 = re.compile(pattern4)
-recomp5 = re.compile(pattern5)
-
-recomp6 = re.compile(pattern6)
 
 '''
     RawInline fuer LaTeX und HTML vorbereiten
@@ -171,156 +94,276 @@ inlineHTML = pf.RawInline(thinSpaceHTML, format="html")
 succHTML = pf.RawInline("", format="html")
 
 
-def latexBlock(first, second):
-    logging.debug("latexBlock: "+"\\mbox{"+first+thinSpaceLaTeX+second+"}"+xspace)
-    return pf.RawInline("\\mbox{"+first+thinSpaceLaTeX+second+"}"+xspace,
-                        format="latex")
+'''
+ Suchmuster (pattern) für die einzelnen Situationen
+
+ pattern1
+ ========
+
+ Dieses Pattern sucht nach x.y. in den Fassungen:
+    x.y. / (x.y. / (x.y.: / (x.y.) / x.y.: ...
+ für alle Buchstaben x und y abdecken.
+ Wichtig ... \D, damit keine Datumsangaben in die Mangel genommen werden!
+ So soll z.B. 15.9.  eben _nicht_ in Muster fallen!
+'''
+pattern1 = "([\(,\[,<,\{]?\w\.)(?:[~|\xa0]?)(\D\.[\),\],>]?[:,\,,\.,\!,\?]?[\),\],\},>]?)"
 
 
-def htmlBlock(first, second):
-    logging.debug("htmlBlock: "+first+thinspaceHTML+second)
-    return pf.RawInline(first+thinspaceHTML+second, format="html")
+'''
+ pattern2
+ ========
+ Dieses Pattern findet alle "/" am Ende einer Zeichenkette.
+
+ Also "Test/" oder "Super/", aber nicht "/" oder "Test/Test"!
+'''
+pattern2 = "([\w|\(|\[|\{]+)\/$"
 
 
-def newBlock(first, second, format):
-    logging.debug("newBlock-format:"+format)
+'''
+ pattern3a/b
+ ===========
+
+ Diesen Pattern pruefen ob der vorherige und nachfolgende Text
+ zu einem halben Leerzeichen fuehrt.
+ Also "z. B." oder "u. a.". Die Leerzeichen werden im pandoc AST
+ zu [String,c="z."][Space][String,c="B."]. Wir testen also beim [Space]
+ das vorherige und nachfolgende String Element.
+'''
+pattern3a = "([\(,\[,<,\{]?\w\.)"
+pattern3b = "^(\w\.[\),\],>]?[:,\,,\.,\!,\?]?[\),\],\},>]?)$"
+
+
+'''
+ pattern4
+ ========
+ Aufspueren von Seitenangaben: "211." und "211-212."
+'''
+pattern4 = "\d+[-\d+]?\.?"
+
+
+'''
+ pattern5
+ ========
+ Aufspueren von Seitenforsetzungsmarkierungen: "211 f." und "211 ff."
+'''
+pattern5 = "^ff?\.?$"
+
+
+'''
+ pattern6
+ ========
+ Aufspueren von Temperaturangaben mit °C oder °F in der Form 21°C korrigieren,
+ ebenso cm, mm, ccm, kg, EUR, km, Meter, m und %
+'''
+pattern6 = "([-|+]?\d+,?-{0,2})(K[.,;:]?|°F[.,;:]?|°C[.,;:]?|€[.,;:]?|T€[.,;:]?|EUR[.,;:]?|Euro[.,;:]?|kg[.,;:]?|g[.,;:]?|km[.,;:]?|m[.,;:]?|Meter[.,;:]?|cm[.,;:]?|mm[.,;:]?|ccm[.,;:]?|\$[.,;:]?|US\-\$[.,;:]?|%[.,;:]?)$"
+
+
+'''
+ Vorkompilieren der Muster:
+'''
+recomp1 = re.compile(pattern1)
+recomp2 = re.compile(pattern2)
+recomp3a = re.compile(pattern3a)
+recomp3b = re.compile(pattern3b)
+recomp4 = re.compile(pattern4)
+recomp5 = re.compile(pattern5)
+recomp6 = re.compile(pattern6)
+
+
+'''
+
+'''
+
+
+def makeLaTeXInline(a):
+    '''
+        Erzeugt aus einer Liste von 2 oder 3 Einträgen den
+        passenden LaTeX-RawInline Code:
+            "\\mbox{x_y}\\xspace{}" bzw. "\\mbox{x_y_z}\\xspace{}"
+        Wobei _ jeweils ein thinSpaceLaTeX ist und x,y,z die 2 bzw. 3
+        Einträge in der Liste
+    '''
+    tmp = beginBox + a[0] + thinSpaceLaTeX + a[1]
+    if len(a) > 2:
+        tmp = tmp + thinSpaceLaTeX + a[2]
+    tmp = tmp + endBox + xspace
+    logging.debug("makeLaTeXInline: " + tmp)
+    return pf.RawInline(tmp, format="latex")
+
+
+def makeHTMLInline(a):
+    '''
+        Erzeugt aus einer Liste von 2 oder 3 Einträgen den
+        passenden HTML-RawInline Code:
+            "x_y" bzw. "x_y_z"
+        Wobei _ jeweils ein thinSpaceHTML ist und x,y,z die 2 bzw. 3
+        Einträge in der Liste
+    '''
+    tmp = a[0]+thinSpaceHTML+a[1]
+    if len(a) > 2:
+        tmp = tmp + thinSpaceHTML + a[2]
+    logging.debug("makeHTMLInline: " + tmp)
+    return pf.RawInline(tmp, format="html")
+
+
+def makeInline(a, format):
+    '''
+        Wählt nach dem Format die passende make(LaTeX/HTML)Inline
+    '''
+    if format in ("latex", "beamer"):
+        return makeLaTeXInline(a)
     if format == "html":
-        tmp = htmlBlock(first, second)
-    if format == "latex":
-        tmp = latexBlock(first, second)
-    if format == "beamer":
-        tmp = latexBlock(first, second)
-    logging.debug("newBlock: "+str(tmp))
-    return tmp
+        return makeHTMLInline(a)
 
 
-def latexBlockThree(first, second, third):
-    logging.debug("latexBlockThree: "+"\\mbox{" + first + thinSpaceLaTeX +
-                        second + thinSpaceLaTeX + third + "}" +
-                        xspace)
-    return pf.RawInline("\\mbox{" + first + thinSpaceLaTeX +
-                        second + thinSpaceLaTeX + third + "}" +
-                        xspace, format="latex")
+def makeLaTeXEscapes(str):
+    if str[0] in ("$", "%"):
+        str = "\\" + str
+    if str[0:4] == "US-$":
+        str = "US-\\"+str[3:]
+    return str
 
 
-def htmlBlockThree(first, second, third,):
-    return pf.RawInline(first + thinSpaceHTML + second +
-                        thinSpaceHTML + third,
-                        format="html")
+def getInline(doc):
+    if doc.format == "html":
+        return inlineHTML
+    if doc.format in ("latex", "beamer"):
+        return inlineLatex
 
 
-def newBlockThree(first, second, third, format):
-    if format == "html":
-        return htmlBlockThree(first, second, third)
-    if format == "latex":
-        return latexBlockThree(first, second, third)
-    if format == "beamer":
-        return latexBlockThree(first, second, third)
+def isThisString(e):
+    return isinstance(e, pf.Str)
+
+
+def isNextString(e):
+    return isThisString(e.next)
+
+
+def isPrevString(e):
+    return isThisString(e.prev)
+
+
+def isPrevAndNextString(e):
+    return isNextString(e) and isPrevString(e)
+
+
+def isThisSpace(e):
+    return isinstance(e, pf.Space)
+
+
+def isNextSpace(e):
+    return isThisSpace(e.next)
+
+
+def isNextParagaph(e):
+    return isinstance(e.next, pf.Para)
+
+
+def isStringEndsWithSlash(str):
+    return str[-1] == "/"
+
+
+def isStringSlashOnly(str):
+    return str == "/"
+
+
+def isPrevNextStringThisSpace(e):
+    return isThisSpace(e) and isPrevAndNextString(e)
+
+
+def handleStringPattern1(elem, doc):
+    splt = recomp1.split(elem.text)
+    logging.debug("Pattern1 text: " + elem.text + " \t split: " + str(splt))
+    if len(splt) == 4:
+        if splt[3] == "":
+            logging.debug("Replacing "+elem.text+" to "+splt[1]+"(Halfspace)"+splt[2]+" at recomp1")
+            return makeInline(splt[1:3], doc.format)
+        else:
+            logging.debug("Replacing "+elem.text+" to "+splt[1]+"(Halfspace)"+splt[2]+"(Halfspace)"+splt[3]+" at recomp1")
+            return makeInline(splt[1:4], doc.format)
+
+
+def handleStringPattern6(elem, doc):
+    splt = recomp6.split(elem.text)
+    logging.debug("Pattern6 text: "+elem.text+" \t "+str(splt))
+    if len(splt) == 4:
+        if splt[0] is not None:
+            splt[1] = splt[0] + splt[1]
+        logging.debug("Replacing "+elem.text+" to "+splt[1]+"(Halfspace)"+splt[2]+" at recomp6")
+        return makeInline(splt[1:3], doc.format)
+
+
+def handleStringPattern2(elem, doc):
+    splt = recomp2.split(elem.text)
+    logging.debug("Pattern2 text: "+elem.text+" \t "+str(splt))
+    if len(splt) == 3 and isinstance(elem.next, pf.Space):
+        logging.debug("Replacing "+elem.text+" to "+splt[1]+"(Halfspace)/ at recomp2")
+        return [pf.Str(splt[1]), getInline(doc), pf.Str("/")]
+
+
+def handleSpaceBetweenStrings(elem, doc):
+    if recomp4.match(elem.next.text):
+        if elem.prev.text == "S.":
+            return getInline(doc)
+    if recomp4.match(elem.prev.text):
+        if recomp5.match(elem.next.text):
+            return getInline(doc)
+
+
+def handleSpacePrevString(elem, doc):
+    if elem.prev.text[-1] == "/":
+        return getInline(doc)
+
+
+def handleSpace(elem, doc):
+    if isPrevAndNextString(elem):
+        ret = handleSpaceBetweenStrings(elem, doc)
+        if ret:
+            return ret
+    if isinstance(elem.next, pf.Str):
+        if elem.next.text[0] == "/":
+            return getInline(doc)
+    ret = handleSpacePrevString(elem, doc)
+    if ret:
+        return ret
+    if (isinstance(elem.next, pf.Str) and
+       len(elem.prev.text) >= 2 and
+       len(elem.next.text) >= 2):
+            mtcha = recomp3a.match(elem.prev.text)
+            mtchb = recomp3b.match(elem.next.text)
+            logging.debug("recomp3a-Text: " +
+                          elem.prev.text +
+                          " \t " + "recomp3b-Text: " +
+                          elem.next.text)
+            if (mtcha and mtchb):
+                logging.debug("Replacing (Space) to (Hlfspace) at recomp3")
+                return getInline(doc)
+
+
+def handleSlashAfterParagraph(elem, doc):
+    if (elem.text == "/") and isinstance(elem.prev, pf.Para):
+        return [getInline(doc), pf.Str("/")]
+
+
+def handleString(elem, doc):
+    ret = handleStringPattern1(elem, doc)
+    if not ret:
+        ret = handleStringPattern6(elem, doc)
+        if not ret:
+            ret = handleStringPattern2(elem, doc)
+            if not ret:
+                ret = handleSlashAfterParagraph(elem, doc)
+    return ret
 
 
 def action(elem, doc):
     '''
-        Der eigentliche Filter
     '''
-    '''
-        Waehle für die Zielsprache (LaTeX / HTML) die
-        passende RawInline Zeile aus
-    '''
-    inline = inlineLatex
-    succ = succLatex
-    if doc.format == "html":
-        inline = inlineHTML
-        succ = succHTML
-
-    '''
-        Der Filter
-    '''
-    if isinstance(elem, pf.Str):
-        txtlen = len(elem.text)
-        '''
-            Hier wird:
-            u.a. / z.B. / d.h. / c.p. / s.u. / s.o.
-            angepasst!
-        '''
-        text = elem.text
-        succtxt = ""
-        pretxt = ""
-
-        '''
-            Pruefung mittels RegEx!
-        '''
-        splt = recomp1.split(elem.text)
-        logging.debug("recomp1-Text: "+elem.text+" \t "+str(splt))
-        if len(splt) == 4:
-            if splt[3] == "":
-                logging.debug("Replacing "+elem.text+" to "+splt[1]+"(Halfspace)"+splt[2]+" at recomp1")
-                return newBlock(splt[1], splt[2], doc.format)
-            else:
-                logging.debug("Replacing "+elem.text+" to "+splt[1]+"(Halfspace)"+splt[2]+"(Halfspace)"+splt[3]+" at recomp1")
-                return newBlockThree(splt[1], splt[2], splt[3], doc.format)
-
-        splt = recomp6.split(elem.text)
-        logging.debug("recomp6-Text: "+elem.text+" \t "+str(splt))
-        if len(splt) == 4:
-            logging.debug("Replacing "+elem.text+" to "+splt[1]+"(Halfspace)"+splt[2]+" at recomp1")
-            return newBlock(splt[1], splt[2], doc.format)
-        '''
-            Hier wird
-            Text/ Text -> Text\,/
-            angepasst!
-        '''
-        splt = recomp2.split(elem.text)
-        logging.debug("recomp2-Text: "+elem.text+" \t "+str(splt))
-        if len(splt) == 3 and isinstance(elem.next, pf.Space):
-            logging.debug("Replacing "+elem.text+" to "+splt[1]+"(Halfspace)/ at recomp2")
-            return [pf.Str(splt[1]), inline, pf.Str("/")]
-
-        if (elem.text == "/") and isinstance(elem.prev, pf.Para):
-            return [inline, pf.Str("/")]
-
-    if isinstance(elem, pf.Space):
-        if (isinstance(elem.prev, pf.Str) and
-            isinstance(elem.next, pf.Str) and
-            recomp4.match(elem.next.text) and
-            elem.prev.text=="S."):
-            return inline
-        if (isinstance(elem.prev, pf.Str) and
-            isinstance(elem.next, pf.Str) and
-            recomp4.match(elem.prev.text) and
-            recomp5.match(elem.next.text)):
-            return inline
-        '''
-            Hier wird
-            Text/ Text -> Text\,/ Text
-            angepasst!
-        '''
-        if (isinstance(elem.next, pf.Str) and elem.next.text[0] == "/"):
-            return inline
-
-        '''
-        '''
-        if (isinstance(elem.prev, pf.Str)):
-            if elem.prev.text[-1] == "/":
-                return inline
-
-
-            '''
-               Hier wird
-               u. a. / z. B. / Z. B. / d. h. / D. h. / u. "A. / c. p. ect.
-               angepasst!
-            '''
-            if (isinstance(elem.next, pf.Str) and
-               len(elem.prev.text) >= 2 and
-               len(elem.next.text) >= 2):
-                    mtcha = recomp3a.match(elem.prev.text)
-                    mtchb = recomp3b.match(elem.next.text)
-                    logging.debug("recomp3a-Text: " +
-                                  elem.prev.text +
-                                  " \t " + "recomp3b-Text: " +
-                                  elem.next.text)
-                    if (mtcha and mtchb):
-                        logging.debug("Replacing (Space) to (Hlfspace) at recomp3")
-                        return inline
+    if isThisSpace(elem):
+        return handleSpace(elem, doc)
+    if isThisString(elem):
+        return handleString(elem, doc)
+    pass
 
 
 def main():
