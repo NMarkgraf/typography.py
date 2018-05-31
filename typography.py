7fd98868deb83a4472e56bb2c48f98e32ab313f3
Norman Markgraf <NMarkgraf@users.noreply.github.com>
2018-05-31 12:15:01 +0200

    Näher an PEP8

--- typography.py
+++ typography.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-'''
+"""
   Quick-Typographie-Filter: typography.py
 
   (C)opyleft in 2017/18 by Norman Markgraf (nmarkgraf@hotmail.com)
@@ -51,50 +51,43 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-'''
-
+"""
 
 import panflute as pf  # panflute fuer den pandoc AST
 import re as re  # re fuer die Regulaeren Ausdruecke
 import logging  # logging fuer die 'typography.log'-Datei
 
-
 # Eine Log-Datei "typography.log" erzeugen um einfacher zu debuggen
 DEBUGLEVEL = logging.ERROR  # DEBUG  oder ERROR or INFO
 logging.basicConfig(filename='typography.log', level=DEBUGLEVEL)
 
-
-'''
+"""
  Halbeleerzeichen für LaTeX und HTML
-'''
+"""
 thinSpaceLaTeX = "\\thinspace{}"  # Schmales Leerzeichen in LaTeX equiv. "\,"
-thinSpaceHTML = "&thinsp;"      # Schmales Leerzeichen in HTML
+thinSpaceHTML = "&thinsp;"  # Schmales Leerzeichen in HTML
 
-
-'''
+"""
  Das Pakete 'xspace' wird benutzt um am Ende der Einfuegung ggf. noch ein
  Leerzeichen abzuhaengen. Das wird hiermit vorbereitet:
-'''
+"""
 xspace = "\\xspace{}"
 
-
-'''
+"""
  LaTeXt-mbox Begin und Ende
-'''
+"""
 beginBox = "\\mbox{"
 endBox = "}"
 
-
-'''
+"""
     RawInline fuer LaTeX und HTML vorbereiten
-'''
+"""
 inlineLatex = pf.RawInline(thinSpaceLaTeX, format="latex")
 succLatex = pf.RawInline(xspace, format="latex")
 inlineHTML = pf.RawInline(thinSpaceHTML, format="html")
 succHTML = pf.RawInline("", format="html")
 
-
-'''
+"""
  Suchmuster (pattern) für die einzelnen Situationen
 
  pattern1
@@ -105,21 +98,19 @@ succHTML = pf.RawInline("", format="html")
  für alle Buchstaben x und y abdecken.
  Wichtig ... \D, damit keine Datumsangaben in die Mangel genommen werden!
  So soll z.B. 15.9.  eben _nicht_ in Muster fallen!
-'''
+"""
 pattern1 = "([\(,\[,<,\{]?\w\.)(?:[~|\xa0]?)(\D\.[\),\],>]?[:,\,,\.,\!,\?]?[\),\],\},>]?)"
 
-
-'''
+"""
  pattern2
  ========
  Dieses Pattern findet alle "/" am Ende einer Zeichenkette.
 
  Also "Test/" oder "Super/", aber nicht "/" oder "Test/Test"!
-'''
+"""
 pattern2 = "([\w|\(|\[|\{]+)\/$"
 
-
-'''
+"""
  pattern3a/b
  ===========
 
@@ -128,39 +119,34 @@ pattern2 = "([\w|\(|\[|\{]+)\/$"
  Also "z. B." oder "u. a.". Die Leerzeichen werden im pandoc AST
  zu [String,c="z."][Space][String,c="B."]. Wir testen also beim [Space]
  das vorherige und nachfolgende String Element.
-'''
+"""
 pattern3a = "([\(,\[,<,\{]?\w\.)"
 pattern3b = "^(\w\.[\),\],>]?[:,\,,\.,\!,\?]?[\),\],\},>]?)$"
 
-
-'''
+"""
  pattern4
  ========
  Aufspueren von Seitenangaben: "211." und "211-212."
-'''
+"""
 pattern4 = "\d+[-\d+]?\.?"
 
-
-'''
+"""
  pattern5
  ========
  Aufspueren von Seitenforsetzungsmarkierungen: "211 f." und "211 ff."
-'''
+"""
 pattern5 = "^ff?\.?$"
 
-
-'''
+"""
  pattern6
  ========
  Aufspueren von Temperaturangaben mit °C oder °F in der Form 21°C korrigieren,
  ebenso cm, mm, ccm, kg, EUR, km, Meter, m und %
-'''
+"""
 pattern6 = "([-|+]?\d+,?-{0,2})(K[.,;:]?|°F[.,;:]?|°C[.,;:]?|€[.,;:]?|T€[.,;:]?|EUR[.,;:]?|Euro[.,;:]?|kg[.,;:]?|g[.,;:]?|km[.,;:]?|m[.,;:]?|Meter[.,;:]?|cm[.,;:]?|mm[.,;:]?|ccm[.,;:]?|\$[.,;:]?|US\-\$[.,;:]?|%[.,;:]?)$"
 
-
-'''
- Vorkompilieren der Muster:
-'''
+"""Vorkompilieren der Muster.
+"""
 recomp1 = re.compile(pattern1)
 recomp2 = re.compile(pattern2)
 recomp3a = re.compile(pattern3a)
@@ -169,20 +155,19 @@ recomp4 = re.compile(pattern4)
 recomp5 = re.compile(pattern5)
 recomp6 = re.compile(pattern6)
 
+"""
 
-'''
-
-'''
+"""
 
 
 def makeLaTeXInline(a):
-    '''
+    """
         Erzeugt aus einer Liste von 2 oder 3 Einträgen den
         passenden LaTeX-RawInline Code:
             "\\mbox{x_y}\\xspace{}" bzw. "\\mbox{x_y_z}\\xspace{}"
         Wobei _ jeweils ein thinSpaceLaTeX ist und x,y,z die 2 bzw. 3
         Einträge in der Liste
-    '''
+    """
     tmp = beginBox + a[0] + thinSpaceLaTeX + a[1]
     if len(a) > 2:
         tmp = tmp + thinSpaceLaTeX + a[2]
@@ -192,14 +177,14 @@ def makeLaTeXInline(a):
 
 
 def makeHTMLInline(a):
-    '''
+    """
         Erzeugt aus einer Liste von 2 oder 3 Einträgen den
         passenden HTML-RawInline Code:
             "x_y" bzw. "x_y_z"
         Wobei _ jeweils ein thinSpaceHTML ist und x,y,z die 2 bzw. 3
         Einträge in der Liste
-    '''
-    tmp = a[0]+thinSpaceHTML+a[1]
+    """
+    tmp = a[0] + thinSpaceHTML + a[1]
     if len(a) > 2:
         tmp = tmp + thinSpaceHTML + a[2]
     logging.debug("makeHTMLInline: " + tmp)
@@ -207,21 +192,23 @@ def makeHTMLInline(a):
 
 
 def makeInline(a, format):
-    '''
-        Wählt nach dem Format die passende make(LaTeX/HTML)Inline
-    '''
+    """Wähle die passende make(LaTeX/HTML)Inline gemäß dem Format aus.
+
+    :param a:
+    :param format:
+    """
     if format in ("latex", "beamer"):
         return makeLaTeXInline(a)
     if format == "html":
         return makeHTMLInline(a)
 
 
-def makeLaTeXEscapes(str):
-    if str[0] in ("$", "%"):
-        str = "\\" + str
-    if str[0:4] == "US-$":
-        str = "US-\\"+str[3:]
-    return str
+def makeLaTeXEscapes(strg):
+    if strg[0] in ("$", "%"):
+        strg = "\\" + strg
+    if strg[0:4] == "US-$":
+        strg = "US-\\" + strg[3:]
+    return strg
 
 
 def getInline(doc):
@@ -259,12 +246,12 @@ def isNextParagaph(e):
     return isinstance(e.next, pf.Para)
 
 
-def isStringEndsWithSlash(str):
-    return str[-1] == "/"
+def isStringEndsWithSlash(strg):
+    return strg[-1] == "/"
 
 
-def isStringSlashOnly(str):
-    return str == "/"
+def isStringSlashOnly(strg):
+    return strg == "/"
 
 
 def isStringAndSlash(elem):
@@ -278,31 +265,42 @@ def isPrevNextStringThisSpace(e):
 
 def handleStringPattern1(elem, doc):
     splt = recomp1.split(elem.text)
-    logging.debug("Pattern1 text: " + elem.text + " \t split: " + str(splt))
+    logging.debug("Pattern1 text: " + elem.text
+                  + " \t split: " + str(splt))
     if len(splt) == 4:
         if splt[3] == "":
-            logging.debug("Replacing "+elem.text+" to "+splt[1]+"(Halfspace)"+splt[2]+" at recomp1")
+            logging.debug("Replacing " + elem.text
+                          + " to " + splt[1] + "(Halfspace)"
+                          + splt[2] + " at recomp1")
             return makeInline(splt[1:3], doc.format)
         else:
-            logging.debug("Replacing "+elem.text+" to "+splt[1]+"(Halfspace)"+splt[2]+"(Halfspace)"+splt[3]+" at recomp1")
+            logging.debug("Replacing " + elem.text
+                          + " to " + splt[1] + "(Halfspace)"
+                          + splt[2] + "(Halfspace)" + splt[3]
+                          + " at recomp1")
             return makeInline(splt[1:4], doc.format)
 
 
 def handleStringPattern6(elem, doc):
     splt = recomp6.split(elem.text)
-    logging.debug("Pattern6 text: "+elem.text+" \t "+str(splt))
+    logging.debug("Pattern6 text: " + elem.text + " \t " + str(splt))
     if len(splt) == 4:
         if splt[0] is not None:
             splt[1] = splt[0] + splt[1]
-        logging.debug("Replacing "+elem.text+" to "+splt[1]+"(Halfspace)"+splt[2]+" at recomp6")
+        logging.debug("Replacing " + elem.text
+                      + " to " + splt[1]
+                      + "(Halfspace)" + splt[2]
+                      + " at recomp6")
         return makeInline(splt[1:3], doc.format)
 
 
 def handleStringPattern2(elem, doc):
     splt = recomp2.split(elem.text)
-    logging.debug("Pattern2 text: "+elem.text+" \t "+str(splt))
+    logging.debug("Pattern2 text: " + elem.text + " \t " + str(splt))
     if len(splt) == 3 and isinstance(elem.next, pf.Space):
-        logging.debug("Replacing "+elem.text+" to "+splt[1]+"(Halfspace)/ at recomp2")
+        logging.debug("Replacing " + elem.text
+                      + " to " + splt[1]
+                      + "(Halfspace)/ at recomp2")
         return [pf.Str(splt[1]), getInline(doc), pf.Str("/")]
 
 
@@ -332,7 +330,7 @@ def handleBetweenLongString(elem, doc):
                   elem.prev.text +
                   " \t " + "recomp3b-Text: " +
                   elem.next.text)
-    if (mtcha and mtchb):
+    if mtcha and mtchb:
         logging.debug("Replacing (Space) to (Hlfspace) at recomp3")
         return getInline(doc)
 
@@ -343,18 +341,18 @@ def handleSpace(elem, doc):
         if ret:
             return ret
     if isStringAndSlash(elem.next):
-            return getInline(doc)
+        return getInline(doc)
     ret = handleSpacePrevString(elem, doc)
     if ret:
         return ret
     if isPrevAndNextString(elem) and isBetweenLongStrings(elem):
-           ret = handleBetweenLongString(elem, doc)
-           if ret:
-               return ret
+        ret = handleBetweenLongString(elem, doc)
+        if ret:
+            return ret
 
 
 def handleSlashAfterParagraph(elem, doc):
-    if (elem.text == "/") and isinstance(elem.prev, pf.Para):
+    if elem.text == "/" and isinstance(elem.prev, pf.Para):
         return [getInline(doc), pf.Str("/")]
 
 
@@ -370,8 +368,8 @@ def handleString(elem, doc):
 
 
 def action(elem, doc):
-    '''
-    '''
+    """
+    """
     if isThisSpace(elem):
         return handleSpace(elem, doc)
     if isThisString(elem):
